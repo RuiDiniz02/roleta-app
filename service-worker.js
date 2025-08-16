c9c34f7cff85d36e7dc2121cb7d87c0ceab69b67
Rui Diniz <rui.maiodiniz@hotmail.com>
2025-08-16 15:46:38 +0100

    first commit

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+// This service worker is intentionally left blank for this simple example.
+// It's required for the app to be considered a PWA.
+self.addEventListener('fetch', (event) => {
+    // For a simple app, we don't need to handle fetch events.
+    // The browser will handle network requests as usual.
+  });
